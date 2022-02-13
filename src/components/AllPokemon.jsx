09442e7ffac1bfbefcde53f9824f4cb191ee3eeb
Robert Woolbright <robwoolbright@gmail.com>
2022-02-13 16:25:25 -0500

    add data being passed around

---
+++ src/components/AllPokemon.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import { PokemonCard } from ".";
+
+const AllPokemon = ({ allPokemon }) => {
+  console.log(allPokemon);
+
+  return (
+    <div>
+      {allPokemon && allPokemon.length
+        ? allPokemon.map((pokemon, idx) => {
+            return (
+              <Fragment
+                key={`Pokemon in main pokemon list ${pokemon.name} ${idx}`}
+              >
+                <PokemonCard pokemon={pokemon} />
+              </Fragment>
+            );
+          })
+        : null}
+    </div>
+  );
+};
+
+export default AllPokemon;
---
+++ src/components/PokemonCard.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const PokemonCard = ({ pokemon }) => {
+  console.log(pokemon);
+
+  const name = pokemon.name;
+  return (
+    <div>
+      <Link to={"/single_pokemon"}>
+        <p>{name}</p>
+        <img src={pokemon.sprites.back_default} />
+      </Link>
+    </div>
+  );
+};
+
+export default PokemonCard;
