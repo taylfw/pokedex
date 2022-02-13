455d9b521bcd935fc6f8806f1130d3e1525f30a1
Robert Woolbright <robwoolbright@gmail.com>
2022-02-13 15:46:49 -0500

    got all thos pokemon!

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const BASE = "https://pokeapi.co/api/v2/pokemon";
 const SPECIES = "https://pokeapi.co/api/v2/pokemon-species";
+const AllPOKEMON = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
 
-const NUMBER = 9;
+const NUMBER = 1;
 
 // REMEMBER THE SLASH!!!
 
@@ -43,6 +44,24 @@ export async function getDescription() {
     return data.flavor_text_entries[0].flavor_text;
   } catch (error) {
     console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllPokemonUrl() {
+  try {
+    const { data } = await axios.get(`${AllPOKEMON}`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getEachPokemon(url) {
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch (error) {
     throw error;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { getPokemon, getSpecies, getSprites, getDescription } from "./api";
+import {
+  getPokemon,
+  getSpecies,
+  getSprites,
+  getDescription,
+  getAllPokemonUrl,
+  getEachPokemon,
+} from "./api";
 
 import {
   BrowserRouter as Router,
@@ -15,6 +22,8 @@ const App = () => {
   const [data, setData] = useState({});
   const [sprites, setSprites] = useState({});
   const [description, setDescription] = useState("");
+  // const [url, setUrl] = useState([]);
+  const [allPokemon, setAllPokemon] = useState([]);
 
   const bulby = async () => {
     const bulbaData = await getPokemon();
@@ -31,14 +40,62 @@ const App = () => {
     bulbaDescription ? setDescription(bulbaDescription) : null;
   };
 
+  // const urlData = async () => {
+  //   const url = await getAllPokemonUrl();
+  //   if (url) {
+  //     setUrl(url.results);
+  //   } else {
+  //     console.log("data didn't come through");
+  //   }
+  // };
+
+  // const allPokeStufftake1 = async () => {
+  //   // console.log("can is see this 1");
+
+  //   // console.log(url, "inside function");
+  //   try {
+  //     const pokeDetails = url.map((poke) => {
+  //       // console.log("can is see this 2");
+  //       const pokemon = getEachPokemon(poke.url);
+  //       return pokemon;
+  //     });
+
+  //     // console.log(pokeDetails, "hey!");
+  //     setAllPokemon(pokeDetails);
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // };
+
+  const allPokeStuff = async () => {
+    const url = await getAllPokemonUrl();
+    const dataResults = url.results;
+
+    const pokeArray = await Promise.all(
+      dataResults.map(async (eachUrl) => {
+        const urlData = eachUrl.url;
+        const pokemon = getEachPokemon(urlData);
+        return pokemon;
+      })
+    );
+    setAllPokemon(pokeArray);
+  };
+
+  console.log(allPokemon);
+
   useEffect(() => {
-    bulby(), bulbySprites(), bulbyDescription();
+    bulby(), bulbySprites(), bulbyDescription(), allPokeStuff();
   }, []);
 
-  console.log(sprites.front_default);
-  console.log(description);
-
   const image = sprites.front_default;
+
+  // trying to get a list of all pokemon
+
+  // console.log(url.results, "url data");
+
+  // console.log(allPokemon, "all pokemon");
+
   return (
     <Router>
       <div id="App">
